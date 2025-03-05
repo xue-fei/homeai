6c6a580e44a994edc67467fe7d2675f21badad2f
xue-fei <awnuxcvbn@163.com>
2025-03-05 18:18:40 +0800

    ollamasharp接本地模型

--- server/Llm.cs
+++ server/Llm.cs
@@ -1,13 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using OllamaSharp;
+using OllamaSharp.Models;
 
 namespace server
 {
-    internal class Llm
+    public class Llm
     {
+        Tts tts;
+        OllamaApiClient ollama;
 
+        public void Start(Tts tts)
+        {
+            this.tts = tts;
+            var uri = new Uri("http://localhost:11434");
+            ollama = new OllamaApiClient(uri, "qwen2:1.5b");
+        }
+
+        public async void RequestAsync(string prompt)
+        {
+            GenerateRequest gr = new GenerateRequest();
+            gr.Prompt = prompt;
+            gr.Stream = false;
+            var resp = ollama.GenerateAsync(gr);
+            await foreach (GenerateResponseStream? stream in resp)
+            {
+                if (stream != null)
+                {
+                    // 如果已结束
+                    if (stream.Done)
+                    {
+                        Console.Write(stream.Response);
+                        if (tts != null)
+                        {
+                            tts.Generate(stream.Response, 1f, 0);
+                        }
+                    }
+                }
+            }
+        }
     }
 }
